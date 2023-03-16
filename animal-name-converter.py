7ee7e0b3cb96f728348a99a8d4337fee518d4d02
aley-nay <38936568+aley-nay@users.noreply.github.com>
2023-03-17 00:37:16 +0300

    Add files via upload

---
+++ animal-name-converter.py
@@ -0,0 +1,30 @@
+hayvanlar={
+    "kedi":"cat",
+    "köpek":"dog",
+    "at":"horse",
+    "kuş":"bird",
+    "papağan":"parrot",
+    "kaplumbağa":"turtle",
+    "tavşan":"rabbit",
+    "aslan":"lion",
+    "sincap":"squirrel",
+    "su samuru":"sea otter",
+    "kanguru":"kangaroo",
+    "kısakuyruklu kanguru":"quokka",
+    "tilki":"fox",
+    "çöl tilkisi":"fennec fox",
+    "kapibara":"capybara",
+    "gelincik":"mink",
+    "keçi":"goat",
+    "rakun":"racoon",
+    "uçan kuskus":"sugar glider"
+}
+
+def cevir(*args, **kwargs):
+    for hayvan in args:
+        print("{} {}".format(hayvan,kwargs[hayvan])) 
+        
+
+while True:
+    kelime=input("hayvan adı gir")
+    cevir(kelime, **hayvanlar)
